2f7ccfdd68dbd374d51be5d7a3b66a27bdbccd6a
黄博 <1048864021@qq.com>
2020-11-30 21:28:24 +0800

    面向对象以及代码复用

---
+++ JS/func_sao/2.js
@@ -0,0 +1,11 @@
+// java 强类型语言
+// js的超集
+var countArea = function (w, h) {
+    return w * h;
+};
+countArea(1, 2);
+// 如何写出优质代码
+// 1. 注释
+// 2. 可读性 for Map，filter，every，some
+// 3. area从js的繁琐到ts的强类型
+// 增长点，写文章，坐下来，开始写
---
+++ JS/func_sao/2.ts
@@ -0,0 +1,15 @@
+// java 强类型语言
+// js的超集
+
+const countArea = (w :number, h :number) :number => {
+  return w * h;
+}
+countArea(1, 2);
+
+
+// 如何写出优质代码
+// 1. 注释
+// 2. 可读性 for Map，filter，every，some
+// 3. area从js的繁琐到ts的强类型
+
+// 增长点，写文章，坐下来，开始写
