a9bbd38a0583f20567306e74d1b08d87cb488398
黄博 <1048864021@qq.com>
2020-11-30 16:30:37 +0800

    es6数组循环、过滤、箭头函数

--- JS/es6/array/1.js
+++ JS/es6/array/1.js
@@ -10,15 +10,19 @@ const ages = [32, 15, 19, 12];
 
 // 命名
 // ages 数组 filter 语义化的表达 过滤
-// 1.计数 for 机械化
+// 1. 计数 for 机械化
 // 2. 表述性好一些 forEach 函数式编程 提升代码可读性
 
   // console.log(age,index,o);
-const adultArr = ages.filter(age => age >= 18);
+// const adultArr = ages.filter(age => age >= 18);
 
+// 有没有大于18岁的元素，有则true
 const adultPresent = ages.some(age => age >= 18)
-// const adultPresent = ages.filter(age => age >= 18);
 console.log(adultPresent);
+// 过滤出所有>= 18的元素
+const adultArr = ages.filter(age => age >= 18);
+console.log(adultArr);
+
 // 所有人满足需求？
 const allOldEnough = ages.every(age => age >= 18)
 console.log(allOldEnough);
