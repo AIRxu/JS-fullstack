ef3c7ce8b484d8d942e7fe186aebc46d560c5f82
AIRxu <1048864021@qq.com>
2021-04-24 20:08:24 +0800

    客户端按照HTTP协议发送请求与服务端解析请求

--- NODE/http-browser-process/client.js
+++ NODE/http-browser-process/client.js
@@ -1,3 +1,25 @@
+// 源端口号 目的地端口号
 const net = require('net');
+// 按照HTTP协议的报文格式来拼接
 
-net.createConnection({port:8088})
+
+class Xmlhttprequest {
+  constructor() {
+    this.POST
+  }
+}
+const client = net.createConnection({port:8088, host:'127.0.0.1'}, () => {
+  let json = JSON.stringify({a:1});
+  client.write('POST / HTTP/1.1\r\n');
+  client.write('HOST:127.0.0.1\r\n');
+  client.write('Content-Type: application/json\r\n');
+  client.write('\r\n');
+  client.write(json);
+  client.write('\r\n');
+})
+client.on('data', (data) => {
+  console.log('receive', data.toString());
+})
+client.on('end', () => {
+  console.log('disconnect');
+})
